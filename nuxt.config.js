e81a49b3afe8e2238f735ea195e657f3eec92ce0
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-04-30 07:34:20 -0700

    Add InstantSearch and plugin etc.

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ["~/plugins/vue-instantsearch"],
   /*
    ** Nuxt.js dev-modules
    */
--- pages/location.vue
+++ pages/location.vue
@@ -1,20 +1,102 @@
 <template>
-  <v-layout column justify-center align-center>
-    <h1>CO-SHOP</h1>
-    <img src="/icon.png" alt="coshop" />
-    <h3>The shopping assistant you didn't know you needed</h3>
-    <v-btn class="btn--cta" nuxt to="/signup">Get Started</v-btn>
-    <v-btn class="btn--alt">Already a member?</v-btn>
+  <v-layout column align-center>
+    <img class="location__image" src="/location.png" alt="location" />
+    <h1 class="location__header">Where are you located?</h1>
+    <v-form class="location__form" lazy-validation>
+      <v-text-field
+        outlined
+        color="green"
+        v-model="locationEmail"
+        type="email"
+        label="email"
+        :rules="[rules.email, rules.required]"
+      ></v-text-field>
+      <v-text-field
+        outlined
+        color="green"
+        v-model="locationPassword"
+        type="password"
+        label="password"
+        :rules="[rules.required]"
+      ></v-text-field>
+      <button @click.stop.prevent="location" class="btn--cta">location</button>
+    </v-form>
+    <h3 class="location__note">NOT A MEMBER?</h3>
+    <nuxt-link to="/signup">
+      <button class="btn--alt">Get started with Co-Shop</button>
+    </nuxt-link>
   </v-layout>
 </template>
 
 <script>
 export default {
-  mounted() {
-    window.console.log("landing page");
+  middleware: "notAuthenticated",
+  data() {
+    return {
+      rules: {
+        required: value => !!value || "Required.",
+        email: v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      }
+    };
+  },
+  computed: {
+    locationEmail: {
+      get() {
+        return this.$store.state.locationEmail;
+      },
+      set(value) {
+        this.$store.commit("auth/setlocationEmail", value);
+      }
+    },
+    locationPassword: {
+      get() {
+        return this.$store.state.locationPassword;
+      },
+      set(value) {
+        this.$store.commit("auth/setlocationPassword", value);
+      }
+    }
+  },
+  methods: {
+    location() {
+      window.console.log("Attempted to location");
+    }
   }
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+@import "../assets/app";
+
+.location {
+  &__header {
+    margin: 10vh 0 5vh 0;
+  }
+
+  &__form {
+    width: 75vw;
+  }
+
+  &__note {
+    text-align: center;
+    padding: 0 10vw;
+    color: gray;
+    margin: 5vh 0 0 0;
+  }
+}
+
+.btn--cta {
+  background: $green;
+  height: 7vh;
+  width: 100%;
+  color: white;
+  font-size: 24px;
+}
+
+.btn--alt {
+  height: 7vh;
+  width: 80vw;
+  color: $green;
+  font-size: 24px;
+}
 </style>
